173d6989a11bdbd59a4ef4746e2677306848570b
Tim Pietrusky <timpietrusky@gmail.com>
2024-02-21 16:29:02 +0100

    feat: integrated archive to create an archive needed during build

---
+++ scripts/archive.mjs
@@ -0,0 +1,30 @@
+import fsp from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+import Seven from "node-7z";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const my7z = path.join(__dirname, "..", "resources", "7zip", "win", "7za.exe");
+
+const sourceFolder = path.join(__dirname, "..", "resources", "python-embedded");
+const outputArchive = path.join(__dirname, "..", "resources", "python-embedded.7z");
+
+const files = await fsp.readdir(sourceFolder);
+
+const archive = Seven.add(
+	outputArchive,
+	files.map(file => path.join(sourceFolder, file)),
+	{
+		$bin: my7z,
+		recursive: true,
+	}
+);
+
+archive.on("end", () => {
+	console.log("Compression finished");
+});
+
+archive.on("error", error => {
+	console.error("Compression error:", error);
+});
