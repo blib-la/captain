76737cfc9e462165cb256601acd883116959a5fa
Gregor Adams <1148334+pixelass@users.noreply.github.com>
2024-02-21 12:04:46 +0100

    test: setup playwright (#48)
    ## Summary
    
    This PR establishes the foundation for end-to-end (E2E) testing in our
    project by integrating Playwright. It's a crucial step towards building
    a tested and stable application, focusing on simplifying our setup to
    kickstart the testing process effectively.
    
    ## Changes
    
    - **Integrated Playwright:** Sets up Playwright for E2E testing,
    enabling us to begin automating tests for our application.
    - **Codebase Cleanup:** Removed unused files and dependencies to
    facilitate a smoother build process and ensure the testing environment
    is focused only on active features.
    - **Preparation for Feature Reintegration:** This cleanup paves the way
    for a structured reintroduction of features, ensuring each is thoroughly
    tested before becoming part of the main codebase again.
    
    ## Rationale
    
    Following a significant refactor and restructuring of our source code,
    this move is aimed at ensuring the new structure not only supports
    development mode but is also testable and buildable. By starting with a
    lean codebase, we aim to methodically reintroduce features, ensuring
    each is compatible with our new structure and passes E2E tests.
    
    ## Next Steps
    
    With the testing framework in place, we will gradually reintroduce
    features, testing each thoroughly to maintain the integrity and
    stability of our application.
    
    ### Issues Closed
    
    - closes #22

--- src/client/pages/[locale]/settings.tsx
+++ src/client/pages/[locale]/settings.tsx
@@ -12,14 +12,11 @@ import Typography from "@mui/joy/Typography";
 import type { InferGetStaticPropsType } from "next";
 import Head from "next/head";
 import { useTranslation } from "next-i18next";
-import { useState } from "react";
 
 import { makeStaticProperties } from "@/ions/i18n/get-static";
 import { ColorModeSelector } from "@/organisms/color-mode-selector";
 import { CustomScrollbars } from "@/organisms/custom-scrollbars";
-import { FolderField } from "@/organisms/folder-field";
 import { LanguageSelect } from "@/organisms/language-select";
-import { PasswordField } from "@/organisms/password-field";
 
 export function UserPreferences() {
 	const { t } = useTranslation(["common"]);
@@ -53,158 +50,6 @@ export function UserPreferences() {
 						<ListItemDecorator sx={{ width: 172, flexShrink: 0 }}>
 							<LanguageSelect />
 						</ListItemDecorator>
-					</ListItem>
-				</List>
-			</CardContent>
-		</Card>
-	);
-}
-
-export function OpenAISettings() {
-	const { t } = useTranslation(["common"]);
-
-	return (
-		<Card variant="soft">
-			<Typography>{t("common:pages.settings.openAiSettings")}</Typography>
-			<CardContent>
-				<List>
-					<ListItem>
-						<ListItemContent>
-							<Typography level="title-sm">{t("common:openAiApiKey")}</Typography>
-							<Typography level="body-sm">
-								{t("common:pages.settings.openAiApiKeyDescription")}
-							</Typography>
-						</ListItemContent>
-						<ListItemDecorator sx={{ width: 288, flexShrink: 0 }}>
-							<PasswordField
-								fullWidth
-								aria-label={t("common:openAiApiKey")}
-								/* //
-								value={openAiApiKey}
-								onChange={event => {
-									setOpenAiApiKey(event.target.value);
-								}}
-								onBlur={event => {
-									window.ipc.fetch(OPENAI_API_KEY, {
-										method: "POST",
-										data: event.target.value,
-									});
-								}} */
-							/>
-						</ListItemDecorator>
-					</ListItem>
-				</List>
-			</CardContent>
-		</Card>
-	);
-}
-
-export function StableDiffusionSettings() {
-	const { t } = useTranslation(["common"]);
-	const [sdSettings, setSdSettings] = useState({ checkpoints: "", loras: "" });
-
-	return (
-		<Card variant="soft">
-			<Typography>{t("common:pages.settings.stableDiffusionSettings")}</Typography>
-			<CardContent>
-				<List>
-					<ListItem>
-						<ListItemContent>
-							<Typography level="title-sm">
-								{t("common:pages.settings.stableDiffusionCheckpoints")}
-							</Typography>
-							<Typography level="body-sm">
-								{t("common:pages.settings.stableDiffusionCheckpointsDescription")}
-							</Typography>
-						</ListItemContent>
-						<ListItemDecorator sx={{ width: 288, flexShrink: 0 }}>
-							<FolderField
-								fullWidth
-								aria-label={t("common:checkpoints")}
-								value={sdSettings.checkpoints}
-								onChange={event => {
-									setSdSettings(previousValue => ({
-										...previousValue,
-										checkpoints: event.target.value,
-									}));
-								}}
-								/* //
-								onSelect={async value => {
-									await window.ipc.fetch(STABLE_DIFFUSION_SETTINGS, {
-										method: "PATCH",
-										data: { checkpoints: value },
-									});
-									setSdSettings(previousValue => ({
-										...previousValue,
-										checkpoints: value,
-									}));
-								}}
-								onBlur={event => {
-									window.ipc.fetch(STABLE_DIFFUSION_SETTINGS, {
-										method: "PATCH",
-										data: { checkpoints: event.target.value },
-									});
-								}} */
-							/>
-						</ListItemDecorator>
-					</ListItem>
-					<ListItem>
-						<ListItemContent>
-							<Typography level="title-sm">
-								{t("common:pages.settings.stableDiffusionLoras")}
-							</Typography>
-							<Typography level="body-sm">
-								{t("common:pages.settings.stableDiffusionLorasDescription")}
-							</Typography>
-						</ListItemContent>
-						<ListItemDecorator sx={{ width: 288, flexShrink: 0 }}>
-							<FolderField
-								fullWidth
-								aria-label={t("common:loras")}
-								value={sdSettings.loras}
-								onChange={event => {
-									setSdSettings(previousValue => ({
-										...previousValue,
-										loras: event.target.value,
-									}));
-								}}
-								/* //
-								onSelect={async value => {
-									await window.ipc.fetch(STABLE_DIFFUSION_SETTINGS, {
-										method: "PATCH",
-										data: { loras: value },
-									});
-									setSdSettings(previousValue => ({
-										...previousValue,
-										loras: value,
-									}));
-								}}
-								onBlur={event => {
-									window.ipc.fetch(STABLE_DIFFUSION_SETTINGS, {
-										method: "PATCH",
-										data: { loras: event.target.value },
-									});
-								}} */
-							/>
-						</ListItemDecorator>
-					</ListItem>
-				</List>
-			</CardContent>
-		</Card>
-	);
-}
-
-export function RunPodSettings() {
-	const { t } = useTranslation(["common"]);
-	return (
-		<Card variant="soft">
-			<Typography>{t("common:pages.settings.runPodSettings")}</Typography>
-			<CardContent>
-				<List>
-					<ListItem>
-						<ListItemContent>
-							<Typography level="title-sm">{t("common:comingSoon")}</Typography>
-						</ListItemContent>
 					</ListItem>
 				</List>
 			</CardContent>
