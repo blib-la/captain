dbb90b3235abac3f5f8a5af2925d03cb47d49364
pixelass <greg@pixelass.com>
2024-02-21 10:13:10 +0100

    test: setup playwright
    removes a bunch of currently unused files to allow building
    uses a KISS approach to allow a test setup

---
+++ playwright.config.ts
@@ -0,0 +1,10 @@
+import type { PlaywrightTestConfig } from "@playwright/test";
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+const config: PlaywrightTestConfig = {
+	testDir: "./playwright",
+};
+
+export default config;
---
+++ playwright/index.test.ts
@@ -0,0 +1,33 @@
+import type { ElectronApplication, Page } from "@playwright/test";
+import { test, expect } from "@playwright/test";
+import { _electron as electron } from "playwright";
+
+let electronApp: ElectronApplication;
+let page: Page;
+
+test.beforeAll(async () => {
+	// Use  package.main
+	electronApp = await electron.launch({ args: ["."] });
+	const isPackaged = await electronApp.evaluate(async ({ app }) => app.isPackaged);
+
+	expect(isPackaged).toBe(false);
+});
+
+test.afterAll(async () => {
+	await electronApp.close();
+});
+
+test("Renders the first page", async () => {
+	page = await electronApp.firstWindow();
+	const title = await page.title();
+	expect(title).toBe("Blibla");
+});
+
+test("Allows switching the language", async () => {
+	page = await electronApp.firstWindow();
+
+	await expect(page.getByTestId("language-selector-list")).toBeVisible();
+	await expect(page.getByText("Deutsch")).toBeVisible();
+	await page.getByText("Deutsch").click();
+	await expect(page.getByText("Sprache")).toBeVisible();
+});
