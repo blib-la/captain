831beace68eaa75081e8596604a97dc014ec14e1
pixelass <greg@pixelass.com>
2024-02-23 17:58:40 +0100

    feat: add basic live painting UI

--- src/client/preload.d.ts
+++ src/client/preload.d.ts
@@ -1,7 +1,7 @@
-import { IpcHandler } from "../main/preload";
+import { IpcHandler } from "../electron/preload";
 
 declare global {
-  interface Window {
-    ipc: IpcHandler;
-  }
+	interface Window {
+		ipc: IpcHandler;
+	}
 }
