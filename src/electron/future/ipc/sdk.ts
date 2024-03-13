3ef441ae295b8a8d83367eb74641f5b12c57a5cd
pixelass <greg@pixelass.com>
2024-03-13 12:11:58 +0100

    feat: action closes prompt

--- src/electron/future/ipc/sdk.ts
+++ src/electron/future/ipc/sdk.ts
@@ -269,12 +269,18 @@ ipcMain.on(
 	}
 );
 
-// Actions
-
-interface FunctionTree {
+/**
+ * Defines a tree structure where each key can either be another `FunctionTree` or a function that takes
+ * a properties object and performs an action. This structure allows for nested, dynamic function calls.
+ */
+export interface FunctionTree {
 	[key: string]: FunctionTree | ((properties: Record<string, unknown>) => void);
 }
 
+/**
+ * An implementation of the `FunctionTree` interface to store functions related to user data management.
+ * It currently includes a `set` function for updating user store properties.
+ */
 export const functions: FunctionTree = {
 	userStore: {
 		set(properties: Record<string, unknown>) {
@@ -287,50 +293,47 @@ export const functions: FunctionTree = {
 	},
 };
 
-export function handleCaptainAction(message: {
-	action: string;
-	payload: VectorStoreDocument["payload"];
-}) {
-	switch (message.action) {
-		case "function": {
-			try {
-				const { id: functionPath, parameters } = message.payload;
-				const function_ = getProperty(functions, functionPath);
-				if (typeof function_ === "function") {
-					function_(parameters ?? {});
-				}
-			} catch (error) {
-				console.log(error);
-			}
-
-			break;
-		}
-
-		default: {
-			break;
-		}
+/**
+ * Handles actions directed at the `captain` entity by invoking functions specified by a function path within the `functions` tree.
+ * The function to be invoked is determined based on the provided `functionPath`, and it is called with the provided `parameters`.
+ *
+ * @param {Object} payload - An object containing the details of the action to be performed.
+ * @param {string} payload.id - The path to the function within the `functions` tree.
+ * @param {Record<string, unknown>} payload.parameters - The parameters to be passed to the function.
+ */
+export function handleCaptainAction({
+	id: functionPath,
+	parameters,
+}: VectorStoreDocument["payload"]) {
+	const function_ = getProperty(functions, functionPath);
+	if (typeof function_ === "function") {
+		function_(parameters ?? {});
 	}
 }
 
+/**
+ * Sets up an IPC listener for `CAPTAIN_ACTION` messages. When a message is received, it attempts to handle
+ * the action specified in the message payload. Supported actions include invoking a function defined in the
+ * `functions` tree. Errors during function invocation are logged, and the sender window is closed upon completion.
+ */
 ipcMain.on(
 	buildKey([ID.CAPTAIN_ACTION]),
 	(event, message: { action: string; payload: unknown }) => {
+		// This log statement is crucial for development and debugging purposes. It outputs the content of
+		// the incoming message to the console, providing immediate visibility into the action being processed.
+		// Retaining this log facilitates a deeper understanding of the action's payload and helps identify
+		// potential issues or anomalies in the data structure or content. It's particularly useful for tracking
+		// the flow of data and ensuring that the expected actions are triggered correctly within the SDK.
 		console.log(message);
 		switch (message.action) {
 			case "function": {
 				try {
-					const { id: functionPath, parameters } = message.payload as {
-						id: string;
-						parameters: Record<string, unknown>;
-					};
-					const function_ = getProperty(functions, functionPath);
-					if (typeof function_ === "function") {
-						function_(parameters);
-					}
+					handleCaptainAction(message.payload as VectorStoreDocument["payload"]);
 				} catch (error) {
 					console.log(error);
 				}
 
+				event.sender.close();
 				break;
 			}
 
