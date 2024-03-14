440f019356fcf5bc4f52307ab67fb889a4025aeb
Tim Pietrusky <timpietrusky@gmail.com>
2024-03-14 16:21:10 +0100

    docs: added comment on why "xenova/transformers" has no types right now

--- src/electron/future/utils/vector-store.ts
+++ src/electron/future/utils/vector-store.ts
@@ -1,5 +1,13 @@
 import fsp from "node:fs/promises";
 
+// The @xenova/transformers package is imported directly from GitHub as it includes
+// certain functionalities that are not available in the npm published version. This package
+// may not have complete type definitions, which can cause TypeScript to raise compilation errors.
+// The use of `@ts-ignore` is necessary here to bypass these TypeScript errors.
+// However, this is a known issue and has been accounted for in our usage of the library.
+// See package.json for the specific version and source of the @xenova/transformers package.
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import { env } from "@xenova/transformers";
 import { globby } from "globby";
 import matter from "gray-matter";
